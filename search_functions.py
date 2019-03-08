4eea41fe2434e74c30583a70eab5406919261ce0
David McGarvey <davidjmcgarvey@gmail.com>
2019-03-08 13:56:42 -0800

    Search funcitons

--- search_functions.py
+++ search_functions.py
@@ -21,8 +21,10 @@ def clear_screen():
 def search_menu():
     """Opens menu with different options for searching entries"""
     clear_screen()
+    message = "What would you like to search by?"
     while True:
-        print("\nWhat would you like to search by?")
+        print(blue_row("="*len(message)))
+        print(message)
         print("\na) By Employee"
               "\nb) By Date"
               "\nc) By Time Spent"
@@ -30,7 +32,7 @@ def search_menu():
               "\ne) Return to Menu\n")
         search_task = input("> ")
         if search_task.lower() == 'a':
-            display_entries(search_employee())
+            search_employee()
         elif search_task.lower() == 'b':
             search_date()
         elif search_task.lower() == 'c':
@@ -43,34 +45,134 @@ def search_menu():
             print(red_err("That was not an option"))
 
 
-def display_entries(search_query=None):
-    """Take entry from database and display it"""
-    entries = Entry.select().order_by(Entry)
-    if search_query == Entry.user:
-        for entry in entries:
-            print("\n" + blue_row("User Name: " + entry.user))
-            print(blue_row("Task Name: " + entry.task))
-            print(blue_row("Task Date: " + str(entry.date)[:-9]))
-            print(blue_row("Task Minutes: " + str(entry.time)))
-            print(blue_row("Task Notes: " + entry.note))
-            print(blue_row("="*34))
+def list_entries(entries):
+    """Takes search query and provides list of dates asscoiated with entries"""
+    print("\nHere are employees who have made an entry:\n")
+    for entry in entries:
+        print("> " + entry.user)
+    action = input("What would you like to do next? Enter 'q' anytime to quit.")
+    if action.lower() != 'q':
+        print("Made it this far...")
+
+
+
+def print_entry(entry):
+    print("\n" + blue_row("User Name: " + entry.user))
+    print(blue_row("Task Name: " + entry.task))
+    print(blue_row("Task Date: " + str(entry.date)[:-9]))
+    print(blue_row("Task Minutes: " + str(entry.time)))
+    print(blue_row("Task Notes: " + entry.note))
+    print(blue_row("=" * 34) + "\n")
+
+
+def print_entries(entries):
+    """Prints all entries"""
+    for entry in entries:
+        print_entry(entry)
+
+
+# def display_entries(search_query=None):
+#     """Take entry from database and display it"""
+#     entries = Entry.select()
+#
+#     results = []
+#     for entry in entries:
+#         if entry.user == search_query:
+#             results.append(entry)
+#             print_entry(entry)
+#
+#         if entry.user != search_query:
+#             print(red_err("Sorry, no match found."))
 
 
 def search_employee():
     """Search database by employee name"""
-    return input("Please type employee's name: ")
+    search = input("Please type desired employee: ")
+    entries = Entry.select()
+
+    results = []
+    for entry in entries:
+        if search == entry.user:
+            result = entry
+            results.append(entry)
+        elif search != entry.user and results == []:
+            result = None
+
+    if result:
+        list_entries(results)
+    else:
+        print(red_err("\nSorry, no match found. Please try again.\n"))
+
+    return None
 
 
 def search_date():
     """Search database by date"""
+    search = input("\nPlease select desired date using YYYY-MM-DD format: ")
+    search = (search + ' 00:00:00')
+    entries = Entry.select()
+    results = []
+
+    for entry in entries:
+        if str(search) == str(entry.date):
+            result = entry
+            results.append(entry)
+        elif search != entry.date and results == []:
+            result = None
+
+    if result:
+        print_entries(results)
+    else:
+        print(red_err("\nSorry, date not found. Please try again."))
+
+    return None
 
 
 def search_time():
     """Search database by time in minutes"""
+    search = input("Please select task time in minutes: ")
+    entries = Entry.select()
+
+    results = []
+    for entry in entries:
+        if str(search) == str(entry.time):
+            result = entry
+            results.append(entry)
+        elif search != entry.time and results == []:
+            result = None
+
+    if result:
+        print_entries(results)
+    else:
+        print(red_err("\nSorry, no match found. Please try again.\n"))
+
+    return None
 
 
 def search_exact():
     """Search database by an exact keyword"""
+    search = input("Please select desired keyword: ")
+    entries = Entry.select()
+
+    results = []
+    for entry in entries:
+        if search in entry.task:
+            result = entry
+            results.append(entry)
+        elif search in entry.note:
+            result = entry
+            results.append(entry)
+        elif search != entry.task and results == []:
+            result = None
+        elif search != entry.note and results == []:
+            result = None
+
+    if result:
+        print_entries(results)
+    else:
+        print(red_err("\nSorry, no keyword found. Please try again.\n"))
+
+    return None
 
 
 
